cf0958482d61ca8454515b5573f89b2a7d77fc2e
Vokler <valerypavlikov@yandex.ru>
2022-02-07 18:46:11 +0200

    added test_context() controller

--- products/views.py
+++ products/views.py
@@ -1,11 +1,24 @@
 from django.shortcuts import render
 
 
-# функции = контроллеры = вьюхи
-
 def index(request):
     return render(request, 'products/index.html')
 
 
 def products(request):
     return render(request, 'products/products.html')
+
+
+def test_context(request):
+    context = {
+        'title': 'GeekShop - Тестовый контекст',
+        'header': 'Welcome!',
+        'username': 'Иван Иванов',
+        'products': [
+            {'name': 'Худи черного цвета с монограммами adidas Originals', 'price': 6090},
+            {'name': 'Синяя куртка The North Face', 'price': 23725},
+            {'name': 'Коричневый спортивный oversized-топ ASOS DESIGN', 'price': 3390},
+        ],
+        'is_promotion': False,
+    }
+    return render(request, 'products/test-context.html', context)
