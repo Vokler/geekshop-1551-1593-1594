7ed361fd169b6fb081e201f746c2108391a52237
Vokler <valerypavlikov@yandex.ru>
2022-02-14 18:02:41 +0200

    removed test-context

--- products/views.py
+++ products/views.py
@@ -16,18 +16,3 @@ def products(request):
         'products': json.load(open(file_path, encoding='utf-8'))
     }
     return render(request, 'products/products.html', context)
-
-
-def test_context(request):
-    context = {
-        'title': 'GeekShop - Тестовый контекст',
-        'header': 'Welcome!',
-        'username': 'Иван Иванов',
-        'products': [
-            {'name': 'Худи черного цвета с монограммами adidas Originals', 'price': 6090},
-            {'name': 'Синяя куртка The North Face', 'price': 23725},
-            {'name': 'Коричневый спортивный oversized-топ ASOS DESIGN', 'price': 3390},
-        ],
-        'is_promotion': False,
-    }
-    return render(request, 'products/test-context.html', context)
