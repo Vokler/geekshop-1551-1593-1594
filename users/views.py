7e4d5f28bf9b7782b348516f5094b847ee6083e4
Vokler <valerypavlikov@yandex.ru>
2022-02-17 18:17:59 +0200

    added displaying messages

--- users/views.py
+++ users/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, HttpResponseRedirect
 from django.contrib import auth
 from django.urls import reverse
+from django.contrib import messages
 
 from users.forms import UserLoginForm, UserRegistrationForm
 
@@ -26,6 +27,7 @@ def registration(request):
         form = UserRegistrationForm(data=request.POST)
         if form.is_valid():
             form.save()
+            messages.success(request, 'Поздравляем! Регистрация прошла успешно.')
             return HttpResponseRedirect(reverse('users:login'))
     else:
         form = UserRegistrationForm()
